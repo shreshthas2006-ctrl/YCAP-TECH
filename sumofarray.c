dac89213686489f151a9a1f835943fee93af422c
shreshthas2006-ctrl <shreshthas2006@gmail.com>
2026-04-02 23:11:11 +0530

    Add sum of array program in sumofarray.c
    Implement a program to sum elements of an array.

---
+++ sumofarray.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a[]={1,2,3,4,5};
+    int addition=0;
+    for(int i=0;i<5;i++){
+        printf("%d ",a[i]);
+       addition=addition+a[i];
+    }
+    printf("Addition = %d",addition);
+}
